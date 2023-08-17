8b029dd75594c7f9827cc2d5d64bd3828f58781d
tejaswini <tejaswinisateesh@gmail.com>
2023-08-17 17:34:28 +0530

    final push

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Places from './components/Places';
 function App() {
   return (
   <>
-  <BrowserRouter >
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <Routes>
     <Route path='/' element={<Login/>}></Route>
     <Route path='/Home' element={<Home/>}></Route>
